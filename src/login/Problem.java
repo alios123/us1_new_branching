d967816c66fb5e48b91d5a7a9ae3b868d0008a25
Aili Wusiman <aliosmancanada@gmail.com>
2022-11-19 20:03:23 -0500

    problem class from feature 10

---
+++ src/login/Problem.java
@@ -0,0 +1,11 @@
+package login;
+
+public class Problem {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+
+        }
+        //this line is important
+    }
+}
